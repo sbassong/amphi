3dfbc5d98bab9b745c4288230b8a994ce9e7d6d3
Sam Bassong <sam.bassong@gmail.com>
2021-08-30 14:10:45 -0700

    imported and set up browerRouter

--- db/index.js
+++ db/index.js
@@ -11,7 +11,7 @@ mongoose
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-// mongoose.set('debug', true)
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db

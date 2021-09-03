47ed771109146724fa1bc9ba13fea93f930d6f71
Sam Bassong <sam.bassong@gmail.com>
2021-09-03 08:49:44 -0700

    setting up heroku

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/amphiDB'
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/amphiDB', {
+  .connect(dbUrl, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const logger = require('morgan')
 const AppRouter = require('./routes/AppRouter')
 const db = require('./db')
+const path = require('path')
+
 
 const PORT = process.env.PORT || 3001
 
@@ -15,5 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
 
 app.use('/api', AppRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
 
 app.listen(PORT, () => console.log(`Server Running On Port:`, PORT))

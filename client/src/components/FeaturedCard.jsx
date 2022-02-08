c1d93b7d7c41a20b6916f7590913af78341107f3
Sam Bassong <sam.bassong@gmail.com>
2022-02-07 17:15:41 -0800

    restructured featured card jsx

--- client/src/components/FeaturedCard.jsx
+++ client/src/components/FeaturedCard.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
 
 const FeaturedCard = ({image, name, id}) => {
-  
+  const backgroundStyle = {
+    backgroundImage: `url(${image})`,
+    width: '100%',
+    height: 'auto'
+  }
+
   return (
     <Link to={`/artists/${id}`} >
     <div className='featured-card'>
-        <div className='featured-cont'><img src={image} alt={name} /></div>
-        <h1 className='featured-h1' >{name}</h1>
+        <div className='bg-img' style={backgroundStyle}></div>
+        <div className='fg-info'>{name}</div>
     </div>
     </Link >
   )

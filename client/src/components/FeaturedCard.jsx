4f45760829f2b50ed7a3b06918a07708ef223d19
Sam Bassong <sam.bassong@gmail.com>
2022-02-07 17:41:04 -0800

    restructured featured section's map logic

--- client/src/components/FeaturedCard.jsx
+++ client/src/components/FeaturedCard.jsx
@@ -3,14 +3,11 @@ import { Link } from 'react-router-dom'
 const FeaturedCard = ({image, name, id}) => {
   const backgroundStyle = {
     backgroundImage: `url(${image})`,
-    width: '100%',
-    height: 'auto'
   }
 
   return (
     <Link to={`/artists/${id}`} >
-    <div className='featured-card'>
-        <div className='bg-img' style={backgroundStyle}></div>
+    <div className='featured-card' style={backgroundStyle}>
         <div className='fg-info'>{name}</div>
     </div>
     </Link >
--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -4,11 +4,11 @@ const FeaturedSection = ({artists, category}) => {
 
   return (  
     <section className='featured-section'>
-      <h1 className='left-h1'>Featured Hip Hop Artists:</h1>
+      <h1 className='left-h1'>Featured {category} artists</h1>
       <div className='featured'>
         {
-          artists.length > 0 && artists.map(artist => (
-            artist.genre.toLowerCase() === category && <FeaturedCard key={artist._id} image={artist.image} name={artist.name} id={artist._id} />
+          artists.length > 0 && artists.filter(artist => artist.genre.toLowerCase() === category).map((artist, index)=> (
+            index < 3 && <FeaturedCard key={artist._id} image={artist.image} name={artist.name} id={artist._id} />
         ))}
       </div>
     </section>

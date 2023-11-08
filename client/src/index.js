0d6026ac5432d8e93643322f1859470014c3d41d
Sam Bassong <sam.bassong@gmail.com>
2023-11-07 17:00:12 -0800

    possible fix for timeout error causing 503 on heroku prod

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-
+const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <React.StrictMode>
--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(logger('dev'))
 app.use('/api', AppRouter)
 
 if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+
   app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/client/build/index.html`))

32d2b42c49710e8087647fcdaee5082ec5c78a3a
Sam Bassong <sam.bassong@gmail.com>
2023-11-07 17:02:35 -0800

    possible fix for timeout error causing 503 on heroku prod

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api', AppRouter)
 if (process.env.NODE_ENV === 'production') {
   const path = require('path')
 
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path?.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    res.sendFile(path?.join(`${__dirname}/client/build/index.html`))
   })
 }
 

78e067cd480d3b049c71c1160cd0c9e099fc1c76
Sam Bassong <sam.bassong@gmail.com>
2024-06-25 11:22:48 -0700

    updated packages and start script

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+let root = createRoot(container);
 root.render(
   <BrowserRouter>
     <React.StrictMode>
